a031c09088668d2cd314449b9b768dd9cf36c59d
Bastien <nisole.bastien@outlook.fr>
2020-03-24 08:41:04 +0100

    First commit

---
+++ Kebab.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApplication1
+{
+    public class Kebab
+    {
+        private bool salade;
+        private bool tomate;
+        private bool oignon;
+        private bool viande;
+
+        public Kebab(bool salade, bool tomate, bool oignon, bool viande)
+        {
+            this.salade = salade;
+            this.tomate = tomate;
+            this.oignon = oignon;
+            this.viande = viande;
+        }
+        
+        public bool IsAllVeggie()
+        {
+            return salade && tomate && oignon;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Kebab kebab = new Kebab(true, true, false, true);
+            Console.Out.WriteLine(kebab.IsAllVeggie());
+        }
+    }
+}
